945c96173d9b55b1f2cb59fadbcec31b106b9d5d
Avi Solomon <genepool99@gmail.com>
2026-06-29 18:30:00 -0700

    Add scheduled refresh add-on options

--- tpms_analyzer/run.sh
+++ tpms_analyzer/run.sh
@@ -4,6 +4,8 @@ set -e
 
 LOG_PATH="$(bashio::config 'log_path')"
 VEHICLE_MAP_PATH="$(bashio::config 'vehicle_map_path')"
+SCHEDULED_REFRESH_ENABLED="$(bashio::config 'scheduled_refresh_enabled')"
+SCHEDULED_REFRESH_TIME="$(bashio::config 'scheduled_refresh_time')"
 SERVICE_PORT="8099"
 
 DATA_DIR="/data"
@@ -16,11 +18,15 @@ export TPMS_VEHICLE_MAP_PATH="${VEHICLE_MAP_PATH}"
 export TPMS_REPORT_PATH="/config/www/rtl_433/tpms_report.html"
 export TPMS_STATUS_PATH="/config/www/rtl_433/tpms_status.json"
 export TPMS_SERVICE_PORT="${SERVICE_PORT}"
+export TPMS_SCHEDULED_REFRESH_ENABLED="${SCHEDULED_REFRESH_ENABLED}"
+export TPMS_SCHEDULED_REFRESH_TIME="${SCHEDULED_REFRESH_TIME}"
 
 bashio::log.info "Starting TPMS Analyzer service"
 bashio::log.info "Using TPMS log path: ${TPMS_LOG_PATH}"
 bashio::log.info "Using TPMS vehicle map path: ${TPMS_VEHICLE_MAP_PATH}"
 bashio::log.info "Using TPMS service port: ${TPMS_SERVICE_PORT}"
+bashio::log.info "Scheduled refresh enabled: ${TPMS_SCHEDULED_REFRESH_ENABLED}"
+bashio::log.info "Scheduled refresh time: ${TPMS_SCHEDULED_REFRESH_TIME}"
 
 cd /app
 exec python3 tpms_service.py
